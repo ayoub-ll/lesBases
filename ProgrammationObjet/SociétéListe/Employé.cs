59ba6ec90c87ad5ce98a438dc6f7df5a0cc5310d
Ayoub LAAROBI <ayoub.laarobi@gmail.com>
2020-09-22 16:02:53 +0200

    SociétéListe

---
+++ ProgrammationObjet/SociétéListe/Employé.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Société
+{
+    class Employé : Personne
+    {
+        protected decimal _Salaire { get; set; }
+
+        public Employé(string nom, string prenom, int age, decimal salaire) : base(nom, prenom, age)
+        {
+            this._Salaire = salaire;
+        }
+
+        public decimal getSalaire()
+        {
+            return this._Salaire;
+        }
+
+        public void setSalaire(decimal salaire)
+        {
+            this._Salaire = salaire;
+        }
+        public override string ToString() => ("Je suis: " + this._Prénom + " " + this._Nom + " et j'ai " + this._Age + " et je touche " + this._Salaire + "€");
+
+        public void Afficher()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
