7fdb840281716bbe3ca1434992ced52b741ee04f
Ayoub LAAROBI <ayoub.laarobi@gmail.com>
2020-09-24 11:37:15 +0200

    societeListe

---
+++ ProgrammationObjet/SociétéListe/ListeChainée.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListeChainée
+{
+    class Element
+    {
+        Object _Objet;
+        Element _Suivant;
+        public Object Objet { get { return _Objet; } set { _Objet = value; } }
+        public Element Suivant { get { return _Suivant; } set { _Suivant = value; } }
+
+        public Element(Object objet)
+        {
+            this._Objet = objet;
+        }
+    }
+
+    class Liste
+    {
+        private Element _Début;
+        private int _NbEléments;
+        public int NbEléments { get { return _NbEléments; } }
+        public Object this[int i] { get {
+                Element elementCourant = this._Début;
+                int position = 0;
+
+                if(i > this.NbEléments)
+                {
+                    Console.WriteLine("Index demandé dépasse les limites de ce tableau");
+                }
+                
+                while (elementCourant.Suivant != null)
+                {
+                    if (position == i)
+                    {
+                        return elementCourant.Objet;
+                    }
+
+                    elementCourant = elementCourant.Suivant;
+                    position++;
+                }
+
+                return elementCourant.Objet;
+            } }
+
+        public Liste()
+        {
+            _Début = null;
+            _NbEléments = 0;
+        }
+
+        public void InsérerDébut(Object objet)
+        {
+            Element nouveauElement = new Element(objet);
+            nouveauElement.Suivant = this._Début;
+            this._Début = nouveauElement;
+            _NbEléments++;
+        }
+
+        public void InsérerFin(Object objet)
+        {
+            Element nouveauElement = new Element(objet);
+            Element dernierElement = this._Début;
+
+            if(dernierElement == null)
+            {
+                this._Début = nouveauElement;
+                _NbEléments++;
+                return;
+            }
+
+            //Recherche du dernier element
+            while(dernierElement.Suivant != null)
+            {
+                dernierElement = dernierElement.Suivant;
+            }
+            dernierElement.Suivant = nouveauElement;
+
+            _NbEléments++;
+        }
+
+        public void Lister()
+        {
+            if(this._Début == null)
+            {
+                Console.WriteLine("Liste vide");
+                Console.ReadLine();
+                return;
+            }
+
+            Element elementCourant = this._Début;
+            while (elementCourant.Suivant != null)
+            {
+                elementCourant = elementCourant.Suivant;
+                Console.WriteLine(elementCourant.ToString());
+            }
+            Console.ReadLine();
+        }
+
+        public void Vider()
+        {
+            Element elementCourant = this._Début;
+            Element elementSuivant = this._Début.Suivant;
+
+            while (elementSuivant != null)
+            {
+                elementSuivant = elementSuivant.Suivant;
+                elementCourant = null;
+            }
+
+            this._Début = null;
+            this._NbEléments = 0;
+        }
+    }
+}
---
+++ ProgrammationObjet/SociétéListe/SociétéListe.cs
@@ -0,0 +1,48 @@
+﻿using ListeChainée;
+using Société;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SociétéListe
+{
+    class GérerSociétéListe
+    {
+        static void Main(String[] args)
+        {
+            ListeChainée.Liste liste = new ListeChainée.Liste();
+
+            liste.InsérerDébut(new Employé("nom1", "prenom1", 1, 1500));
+            liste.InsérerDébut(new Employé("nom2", "prenom2", 2, 1500));
+            liste.InsérerDébut(new Employé("nom3", "prenom3", 3, 1500));
+            liste.InsérerDébut(new Employé("nom4", "prenom4", 4, 1500));
+            liste.InsérerDébut(new Employé("nom5", "prenom5", 5, 1500));
+            liste.InsérerDébut(new Chef("nom6", "prenom4", 4, 1500, "manutention"));
+            liste.InsérerDébut(new Chef("nom7", "prenom5", 5, 1500, "cartons"));
+            liste.InsérerDébut(new Directeur("nom8", "prenom4", 4, 1500, "cartons", "IT SOCIETY"));
+
+            /* Pour vérifier l'indexeur
+            Console.WriteLine(liste[2]);
+            Console.ReadLine();
+            */
+
+            liste.Lister();
+
+            liste.Vider();
+            liste.Lister();
+
+            liste.InsérerFin(new Employé("nom1", "prenom1", 1, 1500));
+            liste.InsérerFin(new Employé("nom2", "prenom2", 2, 1500));
+            liste.InsérerFin(new Employé("nom3", "prenom3", 3, 1500));
+            liste.InsérerFin(new Employé("nom4", "prenom4", 4, 1500));
+            liste.InsérerFin(new Employé("nom5", "prenom5", 5, 1500));
+            liste.InsérerFin(new Chef("nom6", "prenom4", 4, 1500, "manutention"));
+            liste.InsérerFin(new Chef("nom7", "prenom5", 5, 1500, "cartons"));
+            liste.InsérerFin(new Directeur("nom8", "prenom4", 4, 1500, "cartons", "IT SOCIETY"));
+
+            liste.Lister();
+        }
+    }
+}
